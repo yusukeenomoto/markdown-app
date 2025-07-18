582ab989b147635a153c8aa9582cf0d4753b8a10
yusukeenomoto <dev.eeenoooo@gmail.com>
2025-07-19 02:08:19 +0900

    cleanup.sh訂正

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 running_containers=($(docker ps --format '{{.Names}}'))
 if [ "${#running_containers[@]}" -ne 0 ]; then
     for container_name in "${running_containers[@]}"; do   
-        docker stop "${container_name}" && docker rm "${container_name}"
+        sudo docker stop "${container_name}" && sudo docker rm "${container_name}"
     done
 else
     echo 'container dose not exist.'
